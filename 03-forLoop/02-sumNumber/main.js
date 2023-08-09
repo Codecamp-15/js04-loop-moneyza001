7b57639c2abe729df71c843576c42f6704b10e1e
moneyza001 <supalap.ss@gmail.com>
2023-08-09 22:03:44 +0700

    Done all task

--- 03-forLoop/02-sumNumber/main.js
+++ 03-forLoop/02-sumNumber/main.js
@@ -1,32 +1,31 @@
-let sum1 = 0;
-let sum2 = 0;
-let sum3 = 0;
-let sum4 = 0;
-let sum5 = 0;
+let sumNumber = 0;
+let sumEven = 0;
+let sumOdd = 0;
+let sumEvenSquare = 0;
+let sumThirdSqare = 0;
+
 for(let i = 0; i <= 100; i++){
-    sum1 = sum1 + i;
+    sumNumber = sumNumber + i;
     if(i%2 === 0){
-        sum2 = sum2 + i;
+        sumEven = sumEven + i;
     }
     if(i%2 == 1){
-        sum3 = sum3 + i;
+        sumOdd = sumOdd + i;
     }
 }
 
 for(let i = 0; i <= 100; i++){
     if(i % 3 === 0){
-        sum4 = sum4 + i**2;
+        sumThirdSqare = sumThirdSqare + i**2;
     }
     if(i % 2 === 0){
-        sum5 = sum5 + i**2;
+        sumEvenSquare = sumEvenSquare + i**2;
     }
 
 }
-    console.log(`Sum All ${sum1}`)
-    console.log(`Sum Even ${sum2}`)
-    console.log(`Sum ODD ${sum3}`)
-    console.log(sum5)
-
-    console.log(sum5-sum4)
+    console.log(`Sum All ${sumNumber}`)
+    console.log(`Sum Even ${sumEven}`)
+    console.log(`Sum Odd ${sumOdd}`)
+    console.log(sumEvenSquare-sumThirdSqare)
 
     
--- 03-forLoop/05-multiplicationTable/main.js
+++ 03-forLoop/05-multiplicationTable/main.js
@@ -1,5 +1,5 @@
 for(let i = 2; i <= 12; i++){
-    console.log('Start table')
+    console.log(`Start table ${i}`)
     
     for(let j = 1; j <= 12; j++){
         console.log(`${i} * ${j} = ${i * j}`)
